abc0f577d017582c6e59d64b8f1210dd845d9a2a
H-dragon <gksdyd4238@naver.com>
2020-05-03 00:17:11 +0900

    질문하기기능 구현완료

--- src/main/java/net/slipp/web/HttpSessionUtils.java
+++ src/main/java/net/slipp/web/HttpSessionUtils.java
@@ -8,14 +8,14 @@ public class HttpSessionUtils {
 	
 	public static boolean isLoginUser(HttpSession session) {
 		Object sessionedUser = session.getAttribute(USER_SESSION_KEY);
-		if ( sessionedUser == null) {
+		if (sessionedUser == null) {
 			return false;
 		}
 		return true;
 	}
 	
 	public static User getUserFromSession(HttpSession session) {
-		if (isLoginUser(session)) {
+		if (!isLoginUser(session)) {
 			return null;
 		}
 		
